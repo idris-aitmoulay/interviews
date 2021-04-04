07bf9468049047b60469195de78cbb99b9722c5a
Idris AIT MOUALY <idris.aitmoulay@gmail.com>
2021-04-04 16:19:38 +0100

    finalize tooltips.

--- front/.storybook/main.js
+++ front/.storybook/main.js
@@ -1,4 +1,13 @@
+const path = require('path');
 module.exports = {
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
--- front/src/stories/Tooltips.stories.js
+++ front/src/stories/Tooltips.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Tooltips } from '../shared/components'
+
 const Story = () => {
   return (
     <div>
-      hi hi this is story book
+      Tooltips:
+      <br/>
+      fffffffffffffffffffffffffffffffffffffffffffffffff :
+      <Tooltips text={'tooltiped title'}>
+        <div>la vie est belle la vie est belle</div>
+      </Tooltips>
     </div>
   );
 };
