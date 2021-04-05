7ad65d28e5ec867eb138ab7009420e53fe1daa95
Idris AIT MOUALY <idris.aitmoulay@gmail.com>
2021-04-05 03:03:21 +0100

    update error messages

--- back/src/app/shared/config/error.msg.ts
+++ back/src/app/shared/config/error.msg.ts
@@ -5,10 +5,5 @@ export const errorMessages: { [code: string]: string } = {
     'auth/access-token': 'access_token not exist',
     'common/list-empty': 'votre liste est vide, veuillez l’initialiser',
     'common/model-added': 'inscription refusée vérifiez vos données',
-    'auth/access-token-valid': 'Token still valid',
-    'demand/update-status': 'you have problem with all theses fields [ id OR status ]',
-    'demand/update-demand-status': "vous n'etes pas authorise pour faire l'operation suivante",
-    'demand/user-not-authorized': 'you do not have permission to make this action',
-    'demand/update-all': "vous n'avez pas le droit de mettre a jours cette demand",
-    'demand/update-status-done': 'Vous devez saisir la date de livraison'
+    'auth/access-token-valid': 'Token still valid'
 };
