58f3f5abb1442d39b8cf0a082fd111c1f5b29fe7
Idris AIT MOUALY <idris.aitmoulay@gmail.com>
2021-04-18 20:56:42 +0000

    finalize algorithme of schedule planner.

--- src/meeting-schedule-planner.algorithm.ts
+++ src/meeting-schedule-planner.algorithm.ts
@@ -1,7 +1,8 @@
-import { Duration, IntervalInDay, MinMaxInterval, ReservedTimeInDay } from "./utils/entities";
-import { fromString } from "./utils/times.utils";
-import { map } from 'lodash';
-
+import {Duration, Interval, IntervalInDay, MinMaxInterval, ReservedTimeInDay} from "./utils/entities";
+import { fromString, toHHMMString } from "./utils/times.utils";
+import { groupBy, forEach, filter, last, map, get } from 'lodash';
+import { IntervalUtils } from "./utils/intervals.utils";
+import { messageError } from "./utils/constants";
 
 
 export class MeetingSchedulePlannerAlgorithm {
@@ -15,6 +16,71 @@ export class MeetingSchedulePlannerAlgorithm {
     const intDuration = fromString(`${duration.h}:${duration.m}`);
 
 
-    return {} as ReservedTimeInDay;
+    const entriesIntervals = groupBy(entries, 'day');
+    let suggestion: ReservedTimeInDay | typeof undefined = undefined;
+
+    forEach(entriesIntervals, (item: IntervalInDay[], key: string) => {
+      entriesIntervals[key] = IntervalUtils.union(item) as IntervalInDay[];
+      const proposition: IntervalInDay | undefined = MeetingSchedulePlannerAlgorithm.findMeetingInsideIntervals(entriesIntervals[key], [minBoundary, maxBoundary], intDuration);
+      if (proposition) {
+        suggestion = {
+          day: proposition.day + "",
+          startHour: toHHMMString(proposition.start),
+          endHour: toHHMMString(proposition.end)
+        };
+        return false;
+      }
+    });
+
+    if (!suggestion) {
+      throw new Error(messageError.meetingNotFound)
+    }
+
+    return suggestion as ReservedTimeInDay;
+  }
+
+  static findMeetingInsideIntervals(intervalUnions: IntervalInDay[], boundaries: number[], duration: number): IntervalInDay | undefined {
+    const [bStart, bEnd] = boundaries;
+    let founded = undefined;
+    const workingIntervals = filter(intervalUnions, ({ start, end }: IntervalInDay) => start >= bStart || end <= bEnd);
+
+    const [firstElement, ...restIntervals] = workingIntervals;
+
+    if ((bStart + duration) < firstElement.start) {
+      return {
+        day: get(firstElement, "day"),
+        start: bStart,
+        end: (bStart + duration)
+      }
+    }
+
+    let index = 0;
+    forEach(restIntervals, (interval : Interval) => {
+      const end = get(workingIntervals, `${index}.end`, 0);
+      const timeBetween =  interval.start - end;
+      if (duration <= timeBetween)
+      {
+        founded = {
+          day: get(firstElement, "day", 0),
+          start: (end +1),
+          end: (end + duration)
+        };
+        return false
+      }
+      index++;
+    });
+
+    if (!founded) {
+      const lastInterval: IntervalInDay | typeof undefined = last(workingIntervals);
+      if (lastInterval && (lastInterval.end + duration) <= bEnd) {
+        const end =  get(lastInterval, "end", 0);
+        return {
+          day: get(lastInterval,"day", 0),
+          start: end + 1,
+          end: end + duration,
+        }
+      }
+    }
+    return founded;
   }
 }
