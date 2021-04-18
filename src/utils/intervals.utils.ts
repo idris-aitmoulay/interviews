4e007928c65cf0ee5be81c43d7052e8bf92ea30a
Idris AIT MOUALY <idris.aitmoulay@gmail.com>
2021-04-18 20:36:42 +0000

    interval union algorithm

--- src/utils/intervals.utils.ts
+++ src/utils/intervals.utils.ts
@@ -1,8 +1,22 @@
-import {Interval} from "./entities";
+import { forEach } from 'lodash';
+import { Interval } from "./entities";
 
 export class IntervalUtils {
   public static union(intervals: Interval[]): Interval[] {
+    let index = 0;
+    forEach(intervals, (interval: Interval, i: number) => {
+      if (intervals[index].end >=  interval.start)
+      {
+        intervals[index].start = Math.min(intervals[index].start, interval.start);
+        intervals[index].end = Math.max(intervals[index].end, interval.end);
+        intervals[i] = intervals[index];
+      }
+      else {
+        index++;
+        intervals[index] = intervals[i];
+      }
+    });
 
-    return [];
+    return intervals;
   }
 }
